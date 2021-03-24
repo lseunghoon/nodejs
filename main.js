e6e5c3d13fa343d3b67fcf2c58970ca01e955ef2
lseunghoon <lseunghoon>
2021-03-24 16:08:57 +0900

    sql 연동 완료

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ var compression = require('compression');
 var template = require('./lib/template.js');
 var mysql = require('mysql');
 var db = mysql.createConnection({
-  host : 'localhost',
-  user :'root',
-  password : 'tmdgns0070',
-  database : 'testlsh'
+  host: 'localhost',
+  user: 'root',
+  password: 'tmdgns0070',
+  database: 'testlsh'
 });
 db.connect();
 
@@ -29,115 +29,152 @@ app.get('*', (request, response, next) => {
 })
 
 app.get('/', (request, response) => {
-  var title = 'Welcome'
-  var desciption = 'Hello, Nodejs'
-  var list = template.list(request.list);
-  var html = template.html(title, list,
-    `
-      <h2>${title}</h2>
-      ${desciption}
-      <img src = "/images/notebook.jpeg" style = "width:400px; display:block; margin-top:10px;">
-      `,
-    `<a href ="/create">create</a> `);
+  db.query('select * from topic_test', (err, topics) => {
+    var title = 'Welcome'
+    var desciption = 'Hello, Nodejs'
+    var list = template.list(topics);
+    var html = template.html(title, list,
+      `
+        <h2>${title}</h2>
+        ${desciption}
+        <img src = "/images/notebook.jpeg" style = "width:400px; display:block; margin-top:10px;">
+        `,
+      `<a href ="/create">create</a> `);
 
-  response.send(html);
+    response.send(html);
+  })
 })
 
 app.get('/page/:pageId', function (request, response, next) {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+  db.query('select * from topic_test', (err, topics) => {
     if (err) {
-      next(err);
-    } else {
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ['h1']
-      });
-      var list = template.list(request.list);
-      var html = template.html(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-            <a href="/update/${sanitizedTitle}">update</a>
-            <form action="/delete" method="post">
-              <input type="hidden" name="id" value="${sanitizedTitle}">
-              <input type="submit" value="delete">
-            </form>`
-      );
-      response.send(html);
+      throw err;
     }
-  });
+    //해당 id의 데이터만 읽기
+    db.query(`select * from topic_test left join author on topic_test.author_id = author.id where topic_test.id = ?`, [request.params.pageId], (err2, topic) => {
+      if (err2) {
+        throw err2;
+      }
+      //불러온 sql 데이터는 객체이므로 [0]을 해줌
+      var title = topic[0].title
+      var desciption = topic[0].description
+      var list = template.list(topics); //전체 topic 리스트
+      var html = template.html(title, list,
+        `
+          <h2>${title}</h2>
+          ${desciption}
+          <p>by ${topic[0].name}</p>
+          `,
+        `<a href="/create">create</a>
+          <a href="/update/${request.params.pageId}">update</a>
+          <form action="/delete" method="post">
+            <input type="hidden" name="id" value="${request.params.pageId}">
+            <input type="submit" value="delete">
+          </form>`);
+
+      response.send(html);
+    })
+  })
 });
 
-app.get('/create', (request, response) => { //get 방식
-  var title = 'WEB - create'
-  var list = template.list(request.list);
-  var html = template.html(title, list, `
-    <form action="/create" method="post">
-      <p><input type="text" name="title" placeholder="title"></p>
-      <p>
-        <textarea name = "description" placeholder="description"></textarea>
-      </p>
-      <p>
-        <input type="submit" value="Submit">
-      </p>
-    </form>
-    `, '');
+app.get('/create', (request, response) => {
+  //author 테이블에서 데이터 가져와서 author select 만들기
+  db.query('select * from topic_test', (err, topics) => {
+    db.query('select * from author', (err, authors) => {
+      var title = 'Create';
+      var list = template.list(topics);
+      var html = template.html(title, list,
+        `
+      <form action="/create" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name = "description" placeholder="description"></textarea>
+        </p>
+        <p>
+          ${template.authorSelect(authors)}
+        </p>
+        <p>
+          <input type="submit" value="Submit">
+        </p>
+      </form>
+      `, `<a href="/create">create</a>`);
 
-  response.send(html);
+      response.send(html);
+    })
+  })
 });
 
-app.post('/create', (request, response) => { //post방식
+app.post('/create', (request, response) => {
   var post = request.body;
-  var title = post.title;
-  var description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-    response.redirect(`/page/${title}`);
+  //데이터베이스에 데이터 추가하기
+  db.query(`insert into topic_test (title, description, created, author_id) values(?,?,now(), ?)`, [post.title, post.description, post.author /* author_id는 select 태그에서 name속성 */ ], (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      //result.insertId 삽입한 행의 id값으로 리다이렉트
+      response.redirect(`/page/${result.insertId}`);
+    }
   })
 });
 
 app.get('/update/:pageId', (request, response) => {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, desciption) {
-    var title = request.params.pageId;
-    var list = template.list(request.list);
-    var html = template.html(title, list,
-      `
-         <form action="/update" method="post">
-         <input type="hidden" name="id" value="${title}">
-      <p><input type="text" name="title" placeholder="title" value ="${title}"></p>
-      <p>
-        <textarea name = "description" placeholder="description">${desciption}</textarea>
-      </p>
-      <p>
-        <input type="submit" value="Submit">
-      </p>
-    </form>
-         `,
-      `<a href ="/create">create</a> <a href="/update/${title}">update</a>`);
-    response.send(html);
-  });
-})
+  db.query('select * from topic_test', (err, topics) => {
+    if (err) {
+      throw err;
+    }
+    db.query(`select * from topic_test where id = ?`, [request.params.pageId], (err2, topic) => {
+      if (err2) {
+        throw err2;
+      }
+      db.query('select * from author', (err, authors) => {
+        var title = topic[0].title
+        var desciption = topic[0].description
+        var list = template.list(topics);
+        var html = template.html(title, list,
+          `
+           <form action="/update" method="post">
+           <input type="hidden" name="id" value="${topic[0].id}">
+        <p><input type="text" name="title" placeholder="title" value ="${title}"></p>
+        <p>
+          <textarea name = "description" placeholder="description">${desciption}</textarea>
+        </p>
+        <p>
+        ${template.authorSelect(authors, topic[0].author_id)}
+        </p>
+        <p>
+          <input type="submit" value="Submit">
+        </p>
+      </form>
+           `,
+          `<a href ="/create">create</a> <a href="/update/${topic[0].id}">update</a>`);
+
+        response.send(html);
+      })
+    })
+  })
+});
 
 app.post('/update', function (request, response) {
   var post = request.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function (error) {
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-      response.redirect(`/${title}`);
-    })
-  });
+  //데이터베이스 데이터 업데이트
+  db.query(`update topic_test set title = ?, description =?,author_id=? where id = ?`, [post.title, post.description, post.author, post.id], (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      response.redirect(`/page/${post.id}`);
+    }
+  })
 });
 
 app.post('/delete', (request, response) => {
+  //데이터베이스 해당 id 행 삭제
   var post = request.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base; //보안
-  fs.unlink(`data/${filteredId}`, function (err) {
-    response.redirect('/'); //express에서 리다이렉트
-
+  db.query(`DELETE FROM topic_test WHERE id = ?`, [post.id], (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      response.redirect('/');
+    }
   })
 });
 
@@ -146,7 +183,7 @@ app.use((request, response, next) => {
   response.status(404).send('Sorry cant find that!');
 })
 //error handlers
-app.use((err, request, response, next)=>{
+app.use((err, request, response, next) => {
   console.error(err.stack)
   response.status(500).send('Something broke!')
 })
